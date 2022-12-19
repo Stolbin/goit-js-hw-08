f6dafa622508104b72d28c9c52b2924810da11a3
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-19 18:03:19 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,5 @@
 import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
 
 const refs = {
   form: document.querySelector('.feedback-form'),
@@ -6,42 +7,36 @@ const refs = {
   textarea: document.querySelector('textarea'),
 };
 
-const STORAGE_KEY = 'feedback-form-state';
 
 let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onInput, 500));
 
-populateTextarea();
+saveMessage();
 
 function onInput(event) {
   formData[event.target.name] = event.target.value.trim();
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
 function onFormSubmit(event) {
   event.preventDefault();
-
   if (refs.input.value === '' || refs.textarea.value === '') {
       return alert('Заповніть всі поля');
   }  
   event.currentTarget.reset();  
-  console.log(formData);
-  
+  console.log(formData);  
   if (
       refs.input.value === refs.input.value ||
       refs.textarea.value === refs.textarea.value
     ) {
       return alert('Дякуємо');
-  }
-  
+  }  
   formData = {};
   localStorage.removeItem(STORAGE_KEY);
 }
-
-function populateTextarea() {
+function saveMessage() {
   const savedInput = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
   if (savedInput) {
