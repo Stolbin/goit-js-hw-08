92849da79a5cfd017a5ff4c5795f2d3e73aeb48f
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-20 18:06:27 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,13 +10,4 @@ function onPlay(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 }
 
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        break;
-      default:
-        break;
-      }
-    });
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
